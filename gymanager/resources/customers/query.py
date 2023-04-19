74a8194923a69915c50c779564ec7aaa4badd7f7
Lucas Ferreira <lucasferreiraek@gmail.com>
2023-04-19 01:39:31 -0300

    (new) Add schemas file and query functions to customers module

---
+++ gymanager/resources/customers/query.py
@@ -0,0 +1,54 @@
+from flask import jsonify
+from gymanager.models import Customer
+
+from gymanager.extensions.database import db
+from gymanager.utils.checkers import check_cpf
+from gymanager.utils.generators import gen_registration_number
+
+
+def list_customers():
+    customers = Customer.query.all()
+
+    return customers
+
+def get_customer_by_id(customer_id):
+    customer = Customer.query.filter_by(id=customer_id).first()
+
+    return customer
+
+def create_customer(data):
+
+    registration = gen_registration_number()
+
+    customer = Customer(
+        registration=registration,
+        name=data.name,
+        email=data.email,
+        birth_date=data.birth_date,
+        address=data.address,
+        cpf=data.cpf,
+        phone_number=data.phone_number,
+    )
+
+    db.session.add(customer)
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": "could not possible to create customer"})
+
+    return customer
+
+def delete_customer(customer_id):
+    customer = Customer.query.filter_by(id=customer_id).delete()
+
+    if customer:
+        try:
+            db.session.commit()
+            return jsonify({"message": "ok"})
+        except Exception as e:
+            db.session.rollback()
+            return jsonify({"error": "could not possible to delete customer"})
+    else:
+        return jsonify({"error": "could not possible to delete customer"})
---
+++ gymanager/schemas.py
@@ -0,0 +1,15 @@
+from flask_restful import fields
+
+customer_fields = {
+    "id": fields.String,
+    "registration": fields.String,
+    "name": fields.String,
+    "email": fields.String,
+    "birth_date": fields.DateTime,
+    "address": fields.String,
+    "cpf": fields.String,
+    "phone_number": fields.String,
+    "status": fields.Boolean,
+    "created_at": fields.DateTime,
+    "updated_at": fields.DateTime
+}
